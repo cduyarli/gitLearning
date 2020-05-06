452365b38713478a7df854c9b1e5c2f6d8806908
cduyarli <62029916+cduyarli@users.noreply.github.com>
2020-05-06 18:06:41 +0200

    update Sprint.java

--- src/tests/Sprint.java
+++ src/tests/Sprint.java
@@ -3,6 +3,6 @@ package tests;
 public class Sprint {
     public static void main(String[] args) {
         System.out.println("We are here..");
-        System.out.println("Okey";
+        System.out.println("Okey");
     }
 }
