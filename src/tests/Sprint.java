3c85e7d3283e2b849d514826e95a209e793b7751
cduyarli <cduyarli@icloud.com>
2020-05-06 18:45:41 +0200

    Merge branch 'master' of https://github.com/cduyarli/gitLearning
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- src/tests/Sprint.java
+++ src/tests/Sprint.java
@@ -5,6 +5,6 @@ public class Sprint {
     public static void main(String[] args) {
         System.out.println("Hello Winter...");
         System.out.println("Hello");
-         System.out.println("Hi);
+         System.out.println("Hi");
     }
 }
