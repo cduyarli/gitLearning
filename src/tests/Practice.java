08fef16ee2d6fa6bc83f34451bf8e4157f3e388d
cduyarli <cduyarli@icloud.com>
2020-05-12 17:09:11 +0200

    Merge branch 'master' of https://github.com/cduyarli/gitLearning
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -4,5 +4,6 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("we start practice");
         System.out.println("Okey");
+        // stop writing
     }
 }
