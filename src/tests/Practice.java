3f933ca74b3024abb43b152dddf4c7e974915dfd
cduyarli <62029916+cduyarli@users.noreply.github.com>
2020-05-12 17:01:03 +0200

    Update Practice.java

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -4,6 +4,5 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("we start practice");
         System.out.println("Okey");
-        System.out.println("we start practice");
     }
 }
